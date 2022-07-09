6c9b0913b2f2ae6a075769807c6d70d6a69745dc
hammerToFall <lenorka0@hotmail.com>
2022-07-09 10:32:11 +0000

    Subo star wars data modeling

--- src/models.py
+++ src/models.py
@@ -8,23 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50), nullable=False)
+    surname = Column(String(50), nullable=False)
+    email = Column(String(40), nullable=False)
+    password = Column(String(50), nullable=False)
+    subscription_date = Column(Integer, nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    gender = Column(String(250))
+    species = Column(String(250), nullable=False)
+    vehicles = Column(Integer, nullable=False)
+    planet = relationship(User)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(250), nullable=False)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'  
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
 
     def to_dict(self):
         return {}
