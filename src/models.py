1346078b73a4a174dea3d02be0854f25795dfe01
hammerToFall <lenorka0@hotmail.com>
2022-07-15 16:51:31 +0000

    ejercicio modificado

--- src/models.py
+++ src/models.py
@@ -13,37 +13,45 @@ class User(Base):
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False)
-    surname = Column(String(50), nullable=False)
-    email = Column(String(40), nullable=False)
-    password = Column(String(50), nullable=False)
+    name = Column(String(30), nullable=False)
+    surname = Column(String(30), nullable=False)
+    email = Column(String(30), unique = True, nullable=False)
+    password = Column(String(30), unique = True, nullable=False)
     subscription_date = Column(Integer, nullable=False)
+
 
 class Character(Base):
     __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    gender = Column(String(250))
+    name = Column(String(250), unique = True, nullable=False)
+    gender = Column(String(250), nullable=False)
     species = Column(String(250), nullable=False)
     vehicles = Column(Integer, nullable=False)
     planet = relationship(User)
 
+
 class Planet(Base):
     __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250), unique = True, nullable=False)
     population = Column(Integer, nullable=False)
     climate = Column(String(250), nullable=False)
 
-class Favorite(Base):
-    __tablename__ = 'favorite'  
+
+class Character_Favorite(Base):
+    __tablename__ = 'character_favorite'  
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     character_id = Column(Integer, ForeignKey('character.id'))
-    planet_id = Column(Integer, ForeignKey('planet.id'))
 
+    
+class Planet_Favorite(Base):
+    __tablename__ = 'planet_favorite'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
 
     def to_dict(self):
         return {}
